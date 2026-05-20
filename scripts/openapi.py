a6dee9ac2542e72171b5299b07bd5563b0f5182a
Enrico Minack <github@enrico.minack.dev>
2026-05-20 09:45:34 +0200

    Amend OpenApi.read_json type annotation

--- scripts/openapi.py
+++ scripts/openapi.py
@@ -2950,7 +2950,7 @@ class OpenApi:
         )
 
     @staticmethod
-    def read_json(filename: str) -> dict[str, Any]:
+    def read_json(filename: str | Path) -> dict[str, Any]:
         with open(filename) as r:
             return json.load(r)
 
